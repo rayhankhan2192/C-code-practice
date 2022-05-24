fbc97fcc96a76fd74f41244689d74c2d71bbd347
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-05-24 17:22:16 +0600

    Update Practice-1.c

--- Class-2/Practice-1.c
+++ Class-2/Practice-1.c
@@ -1,3 +1,7 @@
+/*If cost price and selling price of an item is input through the keyboard,
+write aprogram to determine whether the seller has made profit or incurred loss.
+Also determine how much profit he made or loss he incurred. */
+
 #include<stdio.h>
 int main()
 {
