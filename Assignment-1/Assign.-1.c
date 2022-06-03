b41dd1e4b698de5872e0e1f3376023af6facb9bf
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-06-04 00:23:27 +0600

    Add files via upload

---
+++ Assignment-1/Assign.-1.c
@@ -0,0 +1,20 @@
+/* Write a program in C to display n terms of natural number and their sum */
+
+#include<stdio.h>
+int main()
+{
+    int num,i,sum=0;
+    printf("Enter the value = ");
+    scanf("%d",&num);
+
+    for(i=1; i<=num; i++)
+    {
+        printf(" %d \t",i);
+        sum = sum+i;
+
+    }
+
+    printf("\nThe first %d numbers sum is = %d\n\n",num,sum);
+
+    return 0;
+}
