5c409cef267a33c5c475646ce02de8ed24509a46
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-06-04 01:43:14 +0600

    c code

---
+++ Assignment-1/Assing.-2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num[10],i,sum=0;
+    printf("Enter 10 numbers = ");
+
+    for(i=0; i<10; i++)
+    {
+        scanf("%d",&num[i]);
+    }
+
+    for(i=0; i<10; i++)
+    {
+        sum = sum + num[i];
+    }
+    printf("\nThe sum is %d\n",sum);
+    printf("Average number is %.2lf\n\n\n",(float)sum/10);
+
+}
