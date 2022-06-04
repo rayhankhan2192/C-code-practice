81ecd7e80d02844a179eceb9e7a0f2a64125853c
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-06-04 18:10:12 +0600

    c code

---
+++ Assignment-1/Assign.-6.c
@@ -0,0 +1,20 @@
+/*Write a program in C to display the n terms of even natural number and their sum .*/
+
+#include<stdio.h>
+int main()
+{
+    int num,i,sum=0,num2;
+    printf("Enter the even value = ");
+    scanf("%d",&num);
+
+
+    for(i=2; i<=num; i+=2)
+    {
+        printf("%d\t",i);
+        sum = sum + i;
+        num2 = num/2;
+    }
+    printf("\nThe sum of 1st %d even numbers is = %d\n",num2,sum);
+
+
+}
