00aae01e3dd2f25545ed211dcd297e983724f461
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-06-05 23:50:17 +0600

    Add files via upload

---
+++ Class-3/Assing.-5.c
@@ -0,0 +1,22 @@
+/*Write a program in C to make such a
+pattern like a pyramid with an asterisk*/
+
+#include<stdio.h>
+int main()
+{
+    int row,col,n;
+    printf("Enter the value for pattern = ");
+    scanf("%d",&n);
+
+    for(row=1; row<=n; row+=1)
+    {
+        for(col=1; col<=row; col=col+1)
+        {
+            printf("* ");
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
+
