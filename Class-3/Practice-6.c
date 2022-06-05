1ded5a036d0514d894875e726c8f94cf070f0385
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-06-05 23:44:38 +0600

    c code

---
+++ Class-3/Practice-6.c
@@ -0,0 +1,27 @@
+/*Print Fibonacci series up to a certain
+number by taking it input from the user.*/
+
+#include<stdio.h>
+int main()
+{
+    int num,i,n1=0,n2=1,n3;
+    printf("Enter the value = ");
+    scanf("%d",&num);
+   //printf("%d %d\n",n1, n2);
+
+    for(i=0; i<num; i++)
+    {
+        if(i<=1)
+        {
+            n3=i;
+        }
+        else{
+        n3 = n1 + n2;
+        n1=n2;
+        n2=n3;
+        }
+        printf("%d ",n3);
+    }
+
+    getch();
+}
