9d670b9b301f3f7ba926b38e790be2f981d875c6
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-06-06 00:39:40 +0600

    c code

---
+++ Assignment-1/Assing.-7.c
@@ -0,0 +1,28 @@
+/*Write a C program to print prime number from 1 to 200*/
+
+#include<stdio.h>
+int main()
+{
+    int num=200,i,j,count;
+    //printf("Enter the value = ");
+    //scanf("%d",num);
+
+    for(i=1; i<=num; i++)
+    {
+        count=0;
+        for(j=2; j<=i/2; j++)
+        {
+            if(i%j==0)
+            {
+                count++;
+                break;
+            }
+        }
+    if(count==0 && i!=1)
+    {
+        printf("%d\t",i);
+    }
+    }
+    getch();
+
+}
