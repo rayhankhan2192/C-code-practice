b17a4d0f946a521a123617c9365c27c99ebd87b7
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-06-06 02:59:03 +0600

    C code

---
+++ Class-4/Lab-1.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n\t\t========== WELCOME TO MATH WORLD ==========\n\n");
+    printf("1. Factorial number\n");
+    printf("2. Prime number\n");
+    printf("3. Even or Odd\n");
+    printf("4. Exit\n\n");
+    int number,num,i,count=0,fact=1;
+    printf("Select an option = ");
+    scanf("%d",&number);
+
+    switch(number)
+    {
+    case 1:
+        printf("\n\n");
+        printf("\t\t========== FIND FACTORIAL NUMBER ==========\n\n");
+        //int fact=1,num,i;
+        printf("Enter the value = ");
+        scanf("%d",&num);
+        for(i=1; i<=num; i++)
+        {
+            fact = fact*i;
+        }
+        printf("\nAnswer : %d! = %d\n ",num,fact);
+        break;
+
+    case 2:
+        printf("\n\n");
+        printf("\t\t========== FIND PRIME NUMBER ==========\n\n");
+        printf("Enter the value = ");
+        scanf("%d",&num);
+        for(i=2; i<num; i++)
+        {
+            if(num%i==0)
+            {
+                count++;
+                break;
+
+            }
+        }
+        if(count==0)
+        {
+            printf("\nAnswer : prime number\n");
+        }
+        else
+        {
+            printf("\nAnswer : Not prime number\n");
+        }
+        break;
+
+    case 3:
+        printf("\n\n");
+        printf("\t\t========== FIND EVEN & ODD NUMBER ==========\n\n");
+        printf("Enter the value = ");
+        scanf("%d",&num);
+        if(num%2==0)
+        {
+            printf("\nAnswer : Even Number\n");
+        }
+        else
+        {
+            printf("\nAnswer : Odd Number\n");
+        }
+        break;
+
+    case 4:
+        printf("\n");
+        printf("Click Cross for exit from page.\n");
+        break;
+    default :
+        printf("\n\t  TRY AGAIN  \n");
+
+    }
+}
+
