0dd46910e8db54b25c33af46266df0ca06c955aa
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-06-06 15:40:17 +0600

    c code

---
+++ Class-5 (Array)/Lab-4.c
@@ -0,0 +1,29 @@
+/*Write a program to find a minimum number from an array*/
+
+#include<stdio.h>
+int main()
+{
+    {
+        printf("\n\t\t========== FIND MINIMUM NUMBER ==========\n\n");
+        int num[100000],n,i;
+        printf("Select how many numbers = ");
+        scanf("%d",&n);
+        printf("\nEnter %d digit here = ",n);
+
+        for(i=0; i<n; i++)
+        {
+            scanf("%d",&num[i]);
+        }
+        int min=num[0];
+
+        for(i=1; i<n; i++)
+        {
+            if(min>num[i])
+                min=num[i];
+        }
+
+        printf("\nMinimum number = %d\n",min);
+    }
+    getch();
+
+}
