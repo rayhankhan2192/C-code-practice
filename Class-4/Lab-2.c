9283da395f3796dcdffe6cc5e7339ff9610aa741
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-06-06 14:29:41 +0600

    c code

---
+++ Class-4/Lab-2.c
@@ -0,0 +1,54 @@
+/*Write a program that input +, - ,* or / and shows the result by taking variable
+input from the user */
+
+#include<stdio.h>
+int main()
+{
+    double num1,num2;
+    char operatore;
+
+    printf("Enter an operatore = +,-,*,/ : ");
+    scanf("%c",&operatore);
+
+     printf("Enter two numbers : ");
+    scanf("%lf %lf",&num1,&num2);
+
+
+    switch(operatore)
+    {
+
+    case '+' :
+        {
+        printf("%.2lf + %.2lf = %.2lf\n",num1,num2,num1 + num2);
+        break;
+        }
+
+    case '-' :
+        {
+            printf("%.2lf - %.2lf = %.2lf\n",num1,num2,num1 - num2);
+            break;
+        }
+
+    case '*' :
+        {
+            printf("%.2lf * %.2lf = %.2lf\n",num1,num2,num1 * num2);
+            break;
+        }
+
+    case '/' :
+        {
+            printf("%.2lf / %.2lf = %.2lf\n",num1,num2,num1 / num2);
+            break;
+        }
+
+    default :
+        {
+           printf("Not a valid operatore\n");
+           printf("Please try again with valid operatore\n");
+        }
+
+    }
+
+
+    getch();
+}
