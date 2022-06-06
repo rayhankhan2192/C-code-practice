7431f3da1fccedc2dff1909cde03ca26485edae1
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-06-06 15:07:22 +0600

    C code

---
+++ Class-5 (Array)/Lab-1.c
@@ -0,0 +1,28 @@
+/*Write a program to sort an array in ascending order.*/
+
+#include<stdio.h>
+int main()
+{
+    int m[]={12,17,3,33,1,23,9};
+    int i,j,temp;
+    for(i=0; i<7; i++)
+    {
+        printf("%d\t",m[i]);
+    }
+    for(i=0; i<7; i++)
+    {
+        for(j=i+1; j<7; j++)
+        {
+            if(m[i]>m[j])
+            {
+                temp=m[i];
+                m[i]=m[j];
+                m[j]=temp;
+            }
+        }
+    }
+    printf("\n\nArray in Ascending order.\n\n");
+    for(i=0; i<7; i++)
+    printf("%d\t",m[i]);
+    printf("\n\n");
+}
