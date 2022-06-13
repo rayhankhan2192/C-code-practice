386e8b536d9a4112bdaf38256aaa2c4a10dc7bcd
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-06-13 21:11:54 +0600

    c code

---
+++ Class-5 (Array)/Lab-6.c
@@ -0,0 +1,23 @@
+/*Than code can reverse the numbers */
+#include<stdio.h>
+int main()
+{
+    
+    int num[100],i,n;
+    printf("How many numbers = ");
+    scanf("%d",&n);
+    printf("\nEnter %d value = ",n);
+
+    for(i=1; i<=n; i++)
+    {
+        scanf("%d",&num[i]);
+    }
+
+    printf("\nReverse number is = ");
+    for(i=n; i>=1; i--)
+    {
+        printf("%d  ",A[i]);
+    }
+    return 0;
+}
+
