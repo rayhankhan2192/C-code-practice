59edf3665866fe431a908299b6e641865709a9b9
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2022-06-21 21:35:44 +0600

    c code

--- SE_121/Problem-5.c
+++ SE_121/Problem-5.c
@@ -1,5 +1,42 @@
 /*Write a C program to check whether a given number is an armstrong number or not */
+
 #include<stdio.h>
+int main()
+{
+    int num,reminder,sum=0,temp,n=0;
+    printf("Enter the value = ");
+    scanf("%d",&num);
+    temp=num;
+   while(temp!=0)
+   {
+       temp = temp/10;
+       n+=1;
+   }
+   temp = n;
+   int temp_t = num;
+   while(temp!=0)
+   {
+       reminder=temp_t%10;
+       sum+=pow(reminder,n);
+       temp_t = temp_t/10;
+       temp--;
+
+   }
+    if(num==sum)
+    {
+        printf("Armstrong Number");
+    }
+    else
+    {
+        printf("Not Armstrong");
+    }
+    return 0;
+}
+
+
+
+
+/* #include<stdio.h>
 int main()
 {
     int num,reminder,sum=0,temp;
@@ -21,4 +58,4 @@ int main()
         printf("Not Armstrong");
     }
     return 0;
-}
+} */
